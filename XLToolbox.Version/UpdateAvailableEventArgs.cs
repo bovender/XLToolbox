b488c83565236fe7897350074b46e4905285dac4
Daniel Kraus <krada@gmx.net>
2014-06-22 05:25:50 +0200

    Add events for Updater class.
    Signaling available update, downloaded file, etc.

--- XLToolbox.Version/UpdateAvailableEventArgs.cs
+++ XLToolbox.Version/UpdateAvailableEventArgs.cs
@@ -10,12 +10,15 @@ namespace XLToolbox.Version
         public SemanticVersion NewVersion { get; set; }
         public string NewVersionInfo { get; set; }
         public Uri DownloadUrl { get; set; }
+        public string Sha1 { get; set; }
+
         public UpdateAvailableEventArgs(SemanticVersion newVersion, string newVersionInfo,
-            Uri downloadUrl)
+            Uri downloadUrl, string sha1)
         {
             NewVersion = newVersion;
             NewVersionInfo = newVersionInfo;
             DownloadUrl = downloadUrl;
+            Sha1 = sha1;
         }
     }
 }
--- XLToolbox.Version/Updater.cs
+++ XLToolbox.Version/Updater.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace XLToolbox.Version
 {
@@ -11,12 +12,46 @@ namespace XLToolbox.Version
     /// Fetches version information from the internet and raises an UpdateAvailable
     /// event if a new version is available for download.
     /// </summary>
+    /// <remarks>
+    /// The current version information resides in a simple text file which contains
+    /// four lines:              e.g.
+    /// 1) Current version       7.0.0-alpha.1
+    /// 2) Download URL          http://sourceforge.net/projects/xltoolbox/files/XL_Toolbox_7.0.0-alpha.1.exe
+    /// 3) Sha1 of executable    1234abcd...
+    /// 4) Version description   This is the first release of the next generation Toolbox
+    /// </remarks>
     public class Updater
     {
         private const string VERSIONINFOURL = "http://xltoolbox.sourceforge.net/version-ng.txt";
         private UpdateAvailableEventArgs UpdateArgs { get; set; }
+        private string FileName { get; set; }
+        private string Sha1 { get; set; }
+
+        /// <summary>
+        /// Signals that an updated version is available for download.
+        /// </summary>
         public event EventHandler<UpdateAvailableEventArgs> UpdateAvailable;
+
+        /// <summary>
+        /// Signals that the version information could not be downloaded from the internet.
+        /// </summary>
         public event EventHandler<DownloadStringCompletedEventArgs> FetchingVersionFailed;
+
+        /// <summary>
+        /// Signals a change in the download process of the executable file. This event is
+        /// chained from WebClient's event with the same name.
+        /// </summary>
+        public event EventHandler<DownloadProgressChangedEventArgs> DownloadProgressChanged;
+
+        /// <summary>
+        /// Signals that the new release has been downloaded, verified and is ready to install.
+        /// </summary>
+        public event EventHandler<UpdateAvailableEventArgs> DownloadInstallable;
+
+        /// <summary>
+        /// Signals that the downloaded file could not be verified.
+        /// </summary>
+        public event EventHandler<UpdateAvailableEventArgs> DownloadFailedVerification;
 
         /// <summary>
         /// Downloads the current version information file asynchronously from the project
@@ -24,29 +59,80 @@ namespace XLToolbox.Version
         /// </summary>
         public void FetchVersionInformation()
         {
-            WebClient web = new WebClient();
-            web.DownloadStringCompleted += web_DownloadStringCompleted;
-            web.DownloadStringAsync(new Uri(VERSIONINFOURL));
+            WebClient downloadTxt = new WebClient();
+            downloadTxt.DownloadStringCompleted += downloadTxt_DownloadStringCompleted;
+            downloadTxt.DownloadStringAsync(new Uri(VERSIONINFOURL));
         }
 
         /// <summary>
-        /// Inspects the downloaded version information
+        /// Downloads the current release from the internet.
+        /// </summary>
+        public void DownloadUpdate(string fileName)
+        {
+            FileName = fileName;
+            WebClient downloadExe = new WebClient();
+            downloadExe.DownloadProgressChanged += downloadExe_DownloadProgressChanged;
+            downloadExe.DownloadFileCompleted += downloadExe_DownloadFileCompleted;
+            downloadExe.DownloadFileAsync(UpdateArgs.DownloadUrl, fileName);
+        }
+
+        public void InstallUpdate()
+        {
+            // Compute the SHA1 again so we know it's current.
+            ComputeSha1();
+            if (Sha1 == UpdateArgs.Sha1)
+            {
+                System.Diagnostics.Process.Start(FileName, "/UPDATE");
+            }
+            else
+            {
+                OnDownloadFailedVerification();
+            }
+        }
+
+        void downloadExe_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            ComputeSha1();
+            if (Sha1 == UpdateArgs.Sha1)
+            {
+                OnDownloadInstallable();
+            }
+            else
+            {
+                OnDownloadFailedVerification();
+                /* throw new DownloadCorruptException(String.Format(
+                    "Checksum of downloaded file {0} does not match expected checksum {1}",
+                    Sha1, UpdateArgs.Sha1)); */
+            };
+        }
+
+        void downloadExe_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (DownloadProgressChanged != null)
+            {
+                DownloadProgressChanged(this, e);
+            }
+        }
+
+        /// <summary>
+        /// Inspects the downloaded version information.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void web_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        void downloadTxt_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
         {
             if (e.Error == null)
             {
                 StringReader r = new StringReader(e.Result);
                 SemanticVersion v = new SemanticVersion(r.ReadLine());
                 Uri url = new Uri(r.ReadLine());
+                string sha1 = r.ReadLine();
                 string info = r.ReadLine();
 
                 // If a new version is available, raise the corresponding event.
                 if (v != SemanticVersion.CurrentVersion())
                 {
-                    UpdateArgs = new UpdateAvailableEventArgs(v, info, url);
+                    UpdateArgs = new UpdateAvailableEventArgs(v, info, url, sha1);
                     OnUpdateAvailable();
                 }
             }
@@ -54,6 +140,22 @@ namespace XLToolbox.Version
             {
                 // Raise an event that signals failure.
                 OnFetchingVersionFailed(e);
+            }
+        }
+
+        protected virtual void OnDownloadInstallable()
+        {
+            if (DownloadInstallable != null)
+            {
+                DownloadInstallable(this, UpdateArgs);
+            }
+        }
+
+        protected virtual void OnDownloadFailedVerification()
+        {
+            if (DownloadFailedVerification != null)
+            {
+                DownloadFailedVerification(this, UpdateArgs);
             }
         }
 
@@ -70,6 +172,28 @@ namespace XLToolbox.Version
             if (FetchingVersionFailed != null)
             {
                 FetchingVersionFailed(this, e);
+            }
+        }
+
+        /// <summary>
+        /// Computes the Sha1 hash of the downloaded file.
+        /// </summary>
+        /// <returns></returns>
+        private void ComputeSha1()
+        {
+            using (FileStream fs = new FileStream(FileName, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    byte[] hash = sha1.ComputeHash(bs);
+                    StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                    Sha1 = formatted.ToString();
+                }
             }
         }
     }
