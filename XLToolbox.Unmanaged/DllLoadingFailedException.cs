3a8d5a1f0d0ee9f817127f7f32d813f109b21bbc
Daniel Kraus <krada@gmx.net>
2014-07-24 21:19:33 +0200

    Implement XLToolbox.Unmanaged assembly for DLL loading.

---
+++ XLToolbox.Unmanaged/DllLoadingFailedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace XLToolbox.Unmanaged
+{
+    [Serializable]
+    public class DllLoadingFailedException : Exception
+    {
+        public DllLoadingFailedException() { }
+        public DllLoadingFailedException(string message) : base(message) { }
+        public DllLoadingFailedException(string message,
+            Exception innerException)
+            : base(message, innerException) { }
+        public DllLoadingFailedException(SerializationInfo info,
+            StreamingContext context)
+            : base(info, context) { }
+    }
+}
