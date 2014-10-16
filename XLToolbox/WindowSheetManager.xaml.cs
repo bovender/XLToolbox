6c8541d298a263f531c208f81572ad4dd952e430
Daniel Kraus <krada@gmx.net>
2014-10-16 15:02:10 +0200

    Use interaction trigger to close Sheet Manager window.
    Obviates the need to use code behind.

--- XLToolbox/WindowSheetManager.xaml.cs
+++ XLToolbox/WindowSheetManager.xaml.cs
@@ -23,10 +23,5 @@ namespace XLToolbox
         {
             InitializeComponent();
         }
-
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
     }
 }
