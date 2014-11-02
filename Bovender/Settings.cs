9684b45bdfcddbc2eda647ba5576f81ca239be0d
Daniel Kraus <krada@gmx.net>
2014-11-02 07:35:29 +0100

    Implement public settings class.

---
+++ Bovender/Settings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bovender
+{
+    /// <summary>
+    /// Provides easy public access to this assembly's settings file,
+    /// e.g. the user's e-mail address used for submitting exception
+    /// reports.
+    /// </summary>
+    /// <remarks>
+    /// Changed properties need to be explicitly made persistant by
+    /// calling <see cref="Save()"/>.
+    /// </remarks>
+    public static class Settings
+    {
+        public string User
+        {
+            get { return Properties.Settings.Default.User; }
+            set
+            {
+                Properties.Settings.Default.User = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return Properties.Settings.Default.Email; }
+            set
+            {
+                Properties.Settings.Default.Email = value;
+            }
+        }
+
+        public bool CcUser
+        {
+            get { return Properties.Settings.Default.CcUser; }
+            set
+            {
+                Properties.Settings.Default.CcUser = value;
+            }
+        }
+
+        public void Save()
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
