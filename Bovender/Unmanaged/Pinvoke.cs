ccdc4542c1e69e9b53f89f36ee8a6fb84430a720
Daniel Kraus <krada@gmx.net>
2014-12-08 06:17:57 +0100

    Implement clipboard replacement that works with EMF files.

---
+++ Bovender/Unmanaged/Pinvoke.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Bovender.Unmanaged
+{
+    /// <summary>
+    /// Wrappers for Win32 API calls.
+    /// </summary>
+    public static class Pinvoke
+    {
+        #region Public methods
+
+        public static void OpenClipboard(IntPtr hWndNewOwner)
+        {
+            if (!Win32_OpenClipboard(hWndNewOwner))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+
+        public static void CloseClipboard()
+        {
+            if (!Win32_CloseClipboard())
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+
+        public static IntPtr GetClipboardData(uint uFormat)
+        {
+             IntPtr result = Win32_GetClipboardData(uFormat);
+             if (result == IntPtr.Zero)
+             {
+                 throw new Win32Exception(Marshal.GetLastWin32Error());
+             }
+             return result;
+        }
+
+        public static IntPtr CopyEnhMetaFile(IntPtr hemfSrc, string lpszFile)
+        {
+            IntPtr result = Win32_CopyEnhMetaFile(hemfSrc, lpszFile);
+            if (result == IntPtr.Zero)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+            return result;
+        }
+
+        public static void DeleteEnhMetaFile(IntPtr hemf)
+        {
+            if (!Win32_DeleteEnhMetaFile(hemf))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+
+        #endregion
+
+        #region Win32 API constants
+
+        public const uint CF_ENHMETAFILE = 14;
+
+        #endregion
+
+        #region Win32 DLL imports
+
+        [DllImport("user32.dll", EntryPoint = "OpenClipboard", SetLastError = true)]
+        static extern bool Win32_OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll", EntryPoint = "CloseClipboard", SetLastError = true)]
+        static extern bool Win32_CloseClipboard();
+
+        [DllImport("user32.dll", EntryPoint = "GetClipboardData", SetLastError = true)]
+        static extern IntPtr Win32_GetClipboardData(uint uFormat);
+
+        [DllImport("gdi32.dll", EntryPoint = "CopyEnhMetaFile", SetLastError = true)]
+        static extern IntPtr Win32_CopyEnhMetaFile(IntPtr hemfSrc, string lpszFile);
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteEnhMetaFile", SetLastError = true)]
+        static extern bool Win32_DeleteEnhMetaFile(IntPtr hemf);
+
+        #endregion
+    }
+}
---
+++ XLToolbox/Export/WorkingClipboard.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Bovender.Unmanaged;
+
+namespace XLToolbox.Export
+{
+    /// <summary>
+    /// Replacement for the Sysem.Windows.Clipboard class that provides
+    /// Metafile-related functions that actually work. Uses Windows P/Invoke!
+    /// </summary>
+    class WorkingClipboard : IDisposable
+    {
+        #region Public methods
+
+        public Metafile GetMetafile()
+        {
+            _emfHandle = Pinvoke.GetClipboardData(Pinvoke.CF_ENHMETAFILE);
+            return new Metafile(_emfHandle, true);
+        }
+
+        #endregion
+
+        #region Constructor, disposal
+
+        public WorkingClipboard()
+        {
+            Pinvoke.OpenClipboard((IntPtr)Excel.Instance.ExcelInstance.Application.Hwnd);
+        }
+
+        ~WorkingClipboard()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Dispose(bool calledFromDispose)
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+                Pinvoke.CloseClipboard();
+                if (_emfHandle != IntPtr.Zero)
+                {
+                    Pinvoke.DeleteEnhMetaFile(_emfHandle);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Private fields
+
+        IntPtr _emfHandle;
+        bool _disposed;
+
+        #endregion
+    }
+}
