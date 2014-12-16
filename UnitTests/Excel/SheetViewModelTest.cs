03ab60da0b88c4fa2f33b7adfd29e5d3c3ad1168
Daniel Kraus <krada@gmx.net>
2014-12-16 18:50:43 +0100

    Add functionality to SheetViewModel.

--- UnitTests/Excel/SheetViewModelTest.cs
+++ UnitTests/Excel/SheetViewModelTest.cs
@@ -16,19 +16,19 @@ namespace XLToolbox.Test.Excel
     class SheetViewModelTest
     {
         [SetUp]
-        public static void SetUp()
+        public void SetUp()
         {
             ExcelInstance.Start();
         }
 
         [TearDown]
-        public static void TearDown()
+        public void TearDown()
         {
             ExcelInstance.Shutdown();
         }
 
         [Test]
-        public static void Properties()
+        public void Properties()
         {
             Worksheet ws = ExcelInstance.Application.Sheets.Add();
             SheetViewModel svm = new SheetViewModel(ws);
@@ -48,13 +48,87 @@ namespace XLToolbox.Test.Excel
         [TestCase("invalid: characters *", false)]
         [TestCase("/more [invalid] characters\\", false)]
         [TestCase("", false)]
-        public static void ValidSheetNames(string testName, bool isValid)
+        public void ValidSheetNames(string testName, bool isValid)
         {
             string s = isValid ? "" : "not ";
             Assert.AreEqual(
                 isValid,
                 SheetViewModel.IsValidName(testName)
             );
+        }
+
+        [Test]
+        public void CountSelectChartSheet()
+        {
+            Workbook wb = ExcelInstance.CreateWorkbook();
+            Worksheet ws = wb.Worksheets.Add();
+            SheetViewModel svm = new SheetViewModel(ws);
+            Assert.IsFalse(svm.SelectCharts(),
+                "SelectCharts() should return false if worksheet does not contain charts.");
+            Assert.IsFalse(svm.SelectShapes(),
+                "SelectGraphicObjects() should return false if worksheet does not contain any.");
+            Chart chart = wb.Charts.Add();
+            svm = new SheetViewModel(chart);
+            Assert.AreEqual(1, svm.CountCharts());
+            Assert.AreEqual(1, svm.CountShapes());
+            Assert.IsTrue(svm.SelectCharts(),
+                "SelectCharts() should return true if sheet is a chart.");
+            Assert.IsTrue(svm.SelectShapes(),
+                "SelectGraphicObjects() should return true if sheet is a chart.");
+        }
+
+        [Test]
+        public void CountSelectChartObjects()
+        {
+            Workbook wb = ExcelInstance.CreateWorkbook();
+            Worksheet ws = wb.Worksheets.Add();
+            ChartObjects cos = ws.ChartObjects();
+            cos.Add(10, 10, 200, 100);
+            cos.Add(250, 10, 200, 100);
+            SheetViewModel svm = new SheetViewModel(ws);
+            Assert.AreEqual(2, svm.CountCharts(), "CountCharts()");
+            Assert.AreEqual(2, svm.CountShapes(), "CountGraphicObjects()");
+            Assert.IsTrue(svm.SelectCharts(),
+                "SelectCharts() should return true if the sheet contains an embedded chart.");
+            Assert.IsTrue(svm.SelectShapes(),
+                "SelectGraphicObjects() should return true if the sheet contains an embedded chart.");
+        }
+
+        [Test]
+        public void CountSelectShapes()
+        {
+            Workbook wb = ExcelInstance.CreateWorkbook();
+            Worksheet ws = wb.Worksheets.Add();
+            Shapes shs = ws.Shapes;
+            shs.AddLine(10, 10, 20, 30);
+            shs.AddLine(50, 50, 20, 30);
+            SheetViewModel svm = new SheetViewModel(ws);
+            Assert.AreEqual(0, svm.CountCharts());
+            Assert.AreEqual(2, svm.CountShapes());
+            Assert.IsFalse(svm.SelectCharts(),
+                "SelectCharts() should return false if the sheet contains only shapes.");
+            Assert.IsTrue(svm.SelectShapes(),
+                "SelectGraphicObjects() should return true if the sheet contains shapes.");
+        }
+
+        [Test]
+        public void CountSelectShapesAndCharts()
+        {
+            Workbook wb = ExcelInstance.CreateWorkbook();
+            Worksheet ws = wb.Worksheets.Add();
+            Shapes shs = ws.Shapes;
+            shs.AddLine(10, 10, 20, 30);
+            shs.AddLine(50, 50, 20, 30);
+            ChartObjects cos = ws.ChartObjects();
+            cos.Add(10, 10, 200, 100);
+            cos.Add(250, 10, 200, 100);
+            SheetViewModel svm = new SheetViewModel(ws);
+            Assert.AreEqual(2, svm.CountCharts());
+            Assert.AreEqual(4, svm.CountShapes());
+            Assert.IsTrue(svm.SelectCharts(),
+                "SelectCharts() should return true if the sheet contains charts and shapes.");
+            Assert.IsTrue(svm.SelectShapes(),
+                "SelectGraphicObjects() should return true if the sheet contains charts and shapes.");
         }
     }
 }
