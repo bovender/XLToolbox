dbbec39770a71d6afffd594e5a9330bedac534c1
Daniel Kraus <krada@gmx.net>
2014-12-16 18:44:15 +0100

    Add test Helpers class.

---
+++ UnitTests/Helpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+
+namespace XLToolbox.UnitTests
+{
+    static class Helpers
+    {
+        public static void CreateSomeCharts(Worksheet worksheet, int number)
+        {
+            Random random = new Random(worksheet.GetHashCode());
+            ChartObjects cos = worksheet.ChartObjects();
+            ChartObject co;
+            SeriesCollection sc;
+            for (int n = 0; n < number; n++)
+            {
+                co = cos.Add(10 + random.Next(30), 10 + random.Next(40),
+                    300 + random.Next(100), 200 + random.Next(50));
+                sc = co.Chart.SeriesCollection();
+                sc.Add(worksheet.Range["A1:A4"]);
+            }
+        }
+
+        public static void CreateSomeShapes(Worksheet worksheet, int number)
+        {
+            Random random = new Random(worksheet.GetHashCode());
+            Shapes shapes = worksheet.Shapes;
+            for (int n = 0; n < number; n++)
+            {
+                shapes.AddLine(10 + random.Next(30), 10 + random.Next(40),
+                    300 + random.Next(100), 200 + random.Next(50));
+            }
+        }
+    }
+}
