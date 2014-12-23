8db3256f8405192784eddaecf4c777e9d11ca53f
Daniel Kraus <krada@gmx.net>
2014-12-23 01:57:56 +0100

    Implement non-UI action trigger NonUiTrigger.

---
+++ Bovender/Mvvm/Actions/MessageActionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Interactivity;
+
+namespace Bovender.Mvvm.Actions
+{
+    public static class MessageActionExtensions
+    {
+        /// <summary>
+        /// Invokes a <see cref="TriggerAction"/> with the specified parameter.
+        /// </summary>
+        /// <param name="action">The <see cref="TriggerAction"/>.</param>
+        /// <param name="parameter">The parameter value.</param>
+        /// <remarks>
+        /// After http://stackoverflow.com/a/12977944/270712
+        /// </remarks>
+        public static void Invoke(this MessageActionBase action, object parameter)
+        {
+            NonUiTrigger trigger = new NonUiTrigger();
+            trigger.Actions.Add(action);
+
+            try
+            {
+                trigger.Invoke(parameter);
+            }
+            finally
+            {
+                trigger.Actions.Remove(action);
+            }
+        }
+
+        /// <summary>
+        /// Invokes a <see cref="TriggerAction"/>.
+        /// </summary>
+        /// <param name="action">The <see cref="TriggerAction"/>.</param>
+        public static void Invoke(this MessageActionBase action)
+        {
+            action.Invoke(null);
+        }
+    }
+}
