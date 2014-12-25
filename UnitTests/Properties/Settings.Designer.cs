d95b8bb7a7925c4c211d7fa8227bde66fe4d0102
Daniel Kraus <krada@gmx.net>
2014-12-25 15:12:05 +0100

    Add methods to store and retrieve objects.

--- UnitTests/Properties/Settings.Designer.cs
+++ UnitTests/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace XLToolbox.UnitTests.Properties {
+namespace XLToolbox.Test.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
