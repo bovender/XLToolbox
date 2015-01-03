d354170f2f35a91b846d5b6ea1dd5ded3144a505
Daniel Kraus <krada@gmx.net>
2015-01-03 14:05:20 +0100

    Tweak ProcessAction and reusable ProcessView.
    Add a CancelButtonText property to the ProcessAction and the
    ProcessMessageContent; make the ProcessView listen to events.

--- Bovender/Mvvm/Actions/ProcessAction.cs
+++ Bovender/Mvvm/Actions/ProcessAction.cs
@@ -9,10 +9,27 @@ using Bovender.Mvvm.Views;
 namespace Bovender.Mvvm.Actions
 {
     /// <summary>
-    /// Invokes a process view.
+    /// Invokes a process view and injects the ProcessMessageContent
+    /// as a view model into it.
     /// </summary>
+    /// <remarks>
+    /// This action cannot inject itself into the view because actions
+    /// are not view models by Bovender's definition. To enable a view
+    /// that invoke this action to set the strings itself, the Caption
+    /// and Message properties of the MessageActionBase parent class
+    /// and a CancelButtonText are written to the message content
+    /// object (if they are not null or empty strings) so that they
+    /// are available in the newly created view that binds the message
+    /// content as its view model.
+    /// </remarks>
     public class ProcessAction : ShowViewAction
     {
+        #region Public properties
+
+        public string CancelButtonText { get; set; }
+
+        #endregion
+
         #region Overrides
 
         /// <summary>
@@ -26,8 +43,9 @@ namespace Bovender.Mvvm.Actions
             ProcessMessageContent pcm = Content as ProcessMessageContent;
             if (pcm != null)
             {
-                pcm.Caption = Caption;
-                pcm.Message = Message;
+                if (!string.IsNullOrEmpty(Caption)) pcm.Caption = Caption;
+                if (!string.IsNullOrEmpty(Message)) pcm.Message = Message;
+                if (!string.IsNullOrEmpty(CancelButtonText)) pcm.CancelButtonText = CancelButtonText;
                 Window view;
                 // Attempt to create a view from the Assembly and View
                 // parameters. If this fails, create a generic ProcessView.
