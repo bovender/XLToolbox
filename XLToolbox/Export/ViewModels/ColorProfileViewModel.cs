743c2d5e3a05dfa835e10ac22b5b5994ed703e22
Daniel Kraus <krada@gmx.net>
2015-01-13 19:46:56 +0100

    Implement color management methods.

--- XLToolbox/Export/ViewModels/ColorProfileViewModel.cs
+++ XLToolbox/Export/ViewModels/ColorProfileViewModel.cs
@@ -28,6 +28,8 @@ using XLToolbox.Export.Lcms;
 namespace XLToolbox.Export.ViewModels
 {
     using cmsHProfile = IntPtr;
+    using cmsHTransform = IntPtr;
+    using cmsUInt32Number = UInt32;
     using cmsBool = Boolean;
 
     /// <summary>
@@ -41,6 +43,8 @@ namespace XLToolbox.Export.ViewModels
         /// <summary>
         /// Creates a new ColorProfileViewModel from a given file, but only
         /// if the color profile's color space is known by the view model.
+        /// Note that the lcms2.dll must have been loaded prior to calling
+        /// this method!
         /// </summary>
         /// <param name="fileName">Full file name of the ICS color profile.</param>
         /// <returns>Instance of ColorProfileViewModel, or null if the color
@@ -75,6 +79,40 @@ namespace XLToolbox.Export.ViewModels
         public ColorSpace ColorSpace { get; private set; }
 
         public string Name { get; private set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Transforms the pixels of a FreeImageBitmap using a standard color profile
+        /// for the bitmap's color space and the current color profile.
+        /// </summary>
+        /// <param name="freeImageBitmap">FreeImageBitmap whose pixels to convert.</param>
+        /// <remarks>
+        /// Since Excel does not support color management at all, conversions are done
+        /// using standard color profiles that LittleCMS provides.
+        /// </remarks>
+        public void TransformFromStandardProfile(FreeImageAPI.FreeImageBitmap freeImageBitmap)
+        {
+            cmsHProfile standardProfile = CreateStandardProfile(freeImageBitmap);
+            cmsHProfile targetProfile = cmsOpenProfileFromFile(GetPathFromName(), "r");
+
+            if (standardProfile == cmsHProfile.Zero)
+            {
+                throw new InvalidOperationException(
+                    "Unable to create standard profile for " + freeImageBitmap.ColorType.ToString());
+            }
+
+            // Create transform with perceptual intent (0) and no special options (0)
+            cmsHTransform t = cmsCreateTransform(
+                standardProfile, GetLcmsPixelFormat(freeImageBitmap),
+                targetProfile, GetLcmsPixelFormat(ColorSpace),
+                0, 0);
+            UInt32 numPixels = (UInt32)freeImageBitmap.Size.Width * (UInt32)freeImageBitmap.Size.Height;
+            cmsDoTransform(t, freeImageBitmap.Bits, freeImageBitmap.Bits, numPixels);
+            cmsDeleteTransform(t);
+        }
 
         #endregion
 
@@ -154,6 +192,75 @@ namespace XLToolbox.Export.ViewModels
                 Lcms.Constants.COLOR_PROFILE_EXTENSION);
         }
 
+        /// <summary>
+        /// Creates a standard color profile for the color space of the given
+        /// <paramref name="freeImageBitmap"/>.
+        /// </summary>
+        /// <param name="freeImageBitmap">FreeImageBitmap for whose color space
+        /// to create a profile.</param>
+        /// <returns>Handle to a standard profile, or zero if no standard profile
+        /// could be created.</returns>
+        private cmsHProfile CreateStandardProfile(FreeImageAPI.FreeImageBitmap freeImageBitmap)
+        {
+            switch (freeImageBitmap.ColorType)
+            {
+                case FreeImageAPI.FREE_IMAGE_COLOR_TYPE.FIC_RGB:
+                    return cmsCreate_sRGBProfile();
+                case FreeImageAPI.FREE_IMAGE_COLOR_TYPE.FIC_RGBALPHA:
+                    return cmsCreate_sRGBProfile();
+                default:
+                    return cmsHProfile.Zero;
+            }
+        }
+
+        /// <summary>
+        /// Converts a ColorSpace value to an LCMS formatter constant, as defined
+        /// in <see cref="XLToolbox.Export.Lcms.Formatters"/>.
+        /// </summary>
+        /// <param name="colorSpace">ColorSpace value to convert.</param>
+        /// <returns>Resulting LCMS formatter constant.</returns>
+        /// <exception cref="InvalidOperationException">if no predefined formatter
+        /// constant exists for the given ColorSpace value.</exception>
+        private cmsUInt32Number GetLcmsPixelFormat(ColorSpace colorSpace)
+        {
+            switch (colorSpace)
+            {
+                case Models.ColorSpace.Cmyk:
+                    return Lcms.Formatters.TYPE_CMYK;
+                case Models.ColorSpace.Rgb:
+                    return Lcms.Formatters.TYPE_RGBA_8;
+                case Models.ColorSpace.GrayScale:
+                    return Lcms.Formatters.GRAY_8;
+                default:
+                    throw new InvalidOperationException(
+                        "No LCMS formatter defined for " + colorSpace.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Converts a FreeImage color type to an LCMS formatter constant, as defined
+        /// in <see cref="XLToolbox.Export.Lcms.Formatters"/>.
+        /// </summary>
+        /// <param name="freeImageBitmap">FreeImageBitmap whose color type to convert.</param>
+        /// <returns>Resulting LCMS formatter constant.</returns>
+        /// <exception cref="InvalidOperationException">if no predefined formatter
+        /// constant exists for the color type of the given FreeImageBitmap.</exception>
+        private cmsUInt32Number GetLcmsPixelFormat(FreeImageAPI.FreeImageBitmap freeImageBitmap)
+        {
+            switch (freeImageBitmap.ColorType)
+            {
+                case FreeImageAPI.FREE_IMAGE_COLOR_TYPE.FIC_CMYK:
+                    return Lcms.Formatters.TYPE_CMYK;
+                case FreeImageAPI.FREE_IMAGE_COLOR_TYPE.FIC_RGBALPHA:
+                    return Lcms.Formatters.TYPE_RGBA_8;
+                case FreeImageAPI.FREE_IMAGE_COLOR_TYPE.FIC_RGB:
+                    return Lcms.Formatters.TYPE_RGB_8;
+                default:
+                    throw new InvalidOperationException(
+                        "No LCMS formatter defined for " + freeImageBitmap.ColorType.ToString());
+            }
+        }
+
         #endregion
 
         #region Private fields
@@ -170,6 +277,25 @@ namespace XLToolbox.Export.ViewModels
 
         [DllImport("lcms2.dll", EntryPoint = "cmsGetColorSpace")]
         private static extern ColorSpaceSignature cmsGetColorSpace(cmsHProfile hProfile);
+
+        [DllImport("lcms2.dll", EntryPoint = "cmsCreate_sRGBProfile")]
+        private static extern cmsHProfile cmsCreate_sRGBProfile();
+
+        [DllImport("lcms2.dll", EntryPoint = "cmsCreateTransform")]
+        private static extern cmsHTransform cmsCreateTransform(
+            cmsHProfile Input, cmsUInt32Number InputFormat,
+            cmsHProfile Output, cmsUInt32Number OutputFormat,
+            cmsUInt32Number Intent, cmsUInt32Number dwFlags);
+
+        [DllImport("lcms2.dll", EntryPoint = "cmsDeleteTransform")]
+        private static extern void cmsDeleteTransform(cmsHTransform hTransform);
+
+        [DllImport("lcms2.dll", EntryPoint = "cmsDoTransform")]
+        private static extern void cmsDoTransform(
+            cmsHTransform hTransform,
+            IntPtr InputBuffer,
+            IntPtr OutputBuffer,
+            cmsUInt32Number	Size);
 
         #endregion
     }
