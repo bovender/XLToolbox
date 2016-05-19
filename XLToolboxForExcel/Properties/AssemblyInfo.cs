71c70ddb06cffae8bbf35c6e984b2976b66cfc96
Daniel Kraus <bovender@bovender.de>
2016-05-19 21:52:43 +0200

    Bump version numbers.

--- XLToolboxForExcel/Properties/AssemblyInfo.cs
+++ XLToolboxForExcel/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.0.23")]
-[assembly: AssemblyFileVersion("7.0.0.23")]
+[assembly: AssemblyVersion("7.0.0.24")]
+[assembly: AssemblyFileVersion("7.0.0.24")]
 
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
